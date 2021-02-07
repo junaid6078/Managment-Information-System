9e635174a936dbea23444dd973a68a46eec58d09
Muhammad junaid khan <mohammadjunaidirfan@gmail.com>
2021-02-07 23:07:51 +0500

    create input method

--- MIS.cpp
+++ MIS.cpp
@@ -1,2 +1,64 @@
 #include<iostream>
+#include<cstring>
+#include<string>
+#include<conio.h>
 using namespace std;
+
+class MIS{
+	public:
+	string citiname; 
+	string citiadd;
+	string papaname; 
+	string papaadd;
+	string childname; 
+	string childadd;
+	int citinic , papanic , childnic;
+
+	
+	
+	void get_data(){
+		cout<<"enter citizen name = ";
+		cin>>citiname;
+		cout<<"enter CNIC number = ";
+		cin>>citinic;
+		cout<<"enter address = ";
+		cin>>citiadd;		
+		cout<<"enter father's name = ";
+		cin>>papaname;
+		cout<<"enter CNIC number = ";
+		cin>>papanic;
+		cout<<"enter address = ";
+		cin>>papaadd;
+		cout<<"enter child name = ";
+		cin>>childname;
+		cout<<"enter CNIC number = ";
+		cin>>childnic;
+		cout<<"enter address = ";
+		cin>>childadd;
+		cin.ignore();
+		
+	}
+	void put_data(){
+		cout<<"=========Search citizen=========\n";
+		cout<<"\ncitizen name = "<<citiname;
+		cout<<"\ncitizen CNIC = "<<citinic;
+		cout<<"\ncitizen address = "<<citiadd;
+		cout<<"\nfather's name = "<<papaname;
+		cout<<"\nfather's CNIC = "<<papanic;
+		cout<<"\nfather's address = "<<papaadd;
+		cout<<"\nchildren's name = "<<childname;
+		cout<<"\nchildren's CNIC = "<<childnic;
+		cout<<"\nchildren's address = "<<childadd;
+		
+	}
+};
+
+int main(){
+	MIS mis;
+	mis.get_data();
+	mis.put_data();
+	
+	
+	
+	
+}
